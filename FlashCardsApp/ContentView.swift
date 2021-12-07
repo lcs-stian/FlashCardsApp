b127bf663a0538681ea98e976195c7793b3cd63f
Suzanne <STian@lcs.on.ca>
2021-12-07 14:16:34 -0500

    Initial Commit

---
+++ FlashCardsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashCardsApp
+//
+//  Created by Suzanne Tian on 2021-12-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlashCardsApp/FlashCardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsAppApp.swift
+//  FlashCardsApp
+//
+//  Created by Suzanne Tian on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
